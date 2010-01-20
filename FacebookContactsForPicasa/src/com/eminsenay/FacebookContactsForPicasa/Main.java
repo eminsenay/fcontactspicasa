db5f8b2ea1e5be7edbf9709030020e8654febaca
bosisler <bosisler@0a007971-7355-4fcb-866b-2c3d4971b1e5>
2010-01-20 23:30:36 +0000

    Switching to Jigloo GUI Builder GUI improvements
    git-svn-id: svn+ssh://svn.code.sf.net/p/fcontactspicasa/code@12 0a007971-7355-4fcb-866b-2c3d4971b1e5

--- FacebookContactsForPicasa/src/com/eminsenay/FacebookContactsForPicasa/Main.java
+++ FacebookContactsForPicasa/src/com/eminsenay/FacebookContactsForPicasa/Main.java
@@ -1,5 +1,12 @@
 package com.eminsenay.FacebookContactsForPicasa;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
 import com.eminsenay.FacebookContactsForPicasa.UI.UIMain;
 
 public class Main 
@@ -7,7 +14,18 @@ public class Main
 
 	public static void main(String args[]) 
 	{
-		UIMain mainForm = new UIMain();
-		mainForm.Open();
+		Display display = new Display();
+	    final Shell shell = new Shell(display);
+	    
+	    shell.setText("Facebook Contacts for Picasa");
+	    final UIMain composite = new UIMain(shell, SWT.NONE);
+	    shell.setLayout(new FillLayout());
+	    shell.pack();
+	    shell.open();
+	    while (!shell.isDisposed()) {
+	      if (!display.readAndDispatch())
+	        display.sleep();
+	    }
+	    display.dispose();
 	}
 }
--- FacebookContactsForPicasa/src/com/eminsenay/FacebookContactsForPicasa/UI/UIMain.java
+++ FacebookContactsForPicasa/src/com/eminsenay/FacebookContactsForPicasa/UI/UIMain.java
@@ -1,108 +1,117 @@
 package com.eminsenay.FacebookContactsForPicasa.UI;
-
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 import com.eminsenay.FacebookContactsForPicasa.Application;
 
-public class UIMain {
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
+public class UIMain extends Composite {
+	private Label labelContactsXmlPath;
+	private Button buttonRun;
+	private Button buttonBrowse;
+	private Text textContactsXmlPath;
 
-	private Shell sShell = null;  //  @jve:decl-index=0:visual-constraint="5,3"
-	private Button BrowseButton = null;
-	private Text ContactsTextBox = null;
-	private Label ContactsXmlPathLabel = null;
-	private Button RunButton = null;
+	public UIMain(Composite parent, int style) {
+		super(parent, style);
+		initialize();
+	}
 
-	/**
-	 * This method initializes sShell
-	 */
-	private void createSShell() {
-		GridData gridData2 = new GridData();
-		gridData2.grabExcessHorizontalSpace = false;
-		gridData2.verticalAlignment = GridData.CENTER;
-		gridData2.horizontalAlignment = GridData.FILL;
-		GridData gridData11 = new GridData();
-		gridData11.horizontalAlignment = GridData.FILL;
-		gridData11.grabExcessHorizontalSpace = true;
-		gridData11.verticalAlignment = GridData.CENTER;
-		GridData gridData1 = new GridData();
-		gridData1.horizontalAlignment = GridData.BEGINNING;
-		gridData1.grabExcessHorizontalSpace = false;
-		gridData1.grabExcessVerticalSpace = false;
-		gridData1.verticalAlignment = GridData.CENTER;
-		GridData gridData = new GridData();
-		gridData.horizontalSpan = 5;
-		gridData.grabExcessHorizontalSpace = true;
-		GridLayout gridLayout = new GridLayout();
-		gridLayout.numColumns = 3;
-		sShell = new Shell(SWT.SHELL_TRIM | SWT.APPLICATION_MODAL);
-		sShell.setText("Facebook Contacts For Picasa");
-		sShell.setSize(new Point(475, 200));
-		sShell.setLayout(gridLayout);
-		ContactsXmlPathLabel = new Label(sShell, SWT.NONE);
-		ContactsXmlPathLabel.setText("Picasa contacts.xml path: ");
-		ContactsTextBox = new Text(sShell, SWT.BORDER);
-		ContactsTextBox.setLayoutData(gridData11);
-		BrowseButton = new Button(sShell, SWT.NONE);
-		BrowseButton.setText("Browse");
-		BrowseButton.setLayoutData(gridData1);
-		Label filler = new Label(sShell, SWT.NONE);
-		Label filler1 = new Label(sShell, SWT.NONE);
-		RunButton = new Button(sShell, SWT.NONE);
-		RunButton.setText("Run");
-		RunButton.setLayoutData(gridData2);
-		RunButton.addSelectionListener(new RunButtonListener());
-		BrowseButton.addSelectionListener(new BrowseButtonListener());
-		sShell.setSize(new Point(300, 200));
-		ContactsTextBox.setLayoutData(gridData);
+	private void initialize() {
+		FormLayout thisLayout = new FormLayout();
+		this.setLayout(thisLayout);
+		{
+			buttonRun = new Button(this, SWT.PUSH | SWT.CENTER);
+			FormData buttonRunLData = new FormData();
+			buttonRunLData.top =  new FormAttachment(0, 1000, 41);
+			buttonRunLData.width = 47;
+			buttonRunLData.height = 23;
+			buttonRunLData.right =  new FormAttachment(1000, 1000, -12);
+			buttonRun.setLayoutData(buttonRunLData);
+			buttonRun.setText("Run");
+			buttonRun.addSelectionListener(new SelectionAdapter() {
+				public void widgetSelected(SelectionEvent evt) {
+					buttonRunWidgetSelected(evt);
+				}
+			});
+		}
+		{
+			buttonBrowse = new Button(this, SWT.PUSH | SWT.CENTER);
+			FormData buttonBrowseLData = new FormData();
+			buttonBrowseLData.width = 47;
+			buttonBrowseLData.height = 23;
+			buttonBrowseLData.top =  new FormAttachment(0, 1000, 12);
+			buttonBrowseLData.right =  new FormAttachment(1000, 1000, -11);
+			buttonBrowse.setLayoutData(buttonBrowseLData);
+			buttonBrowse.setText("Browse");
+			buttonBrowse.addSelectionListener(new SelectionAdapter() {
+				public void widgetSelected(SelectionEvent evt) {
+					buttonBrowseWidgetSelected(evt);
+				}
+			});
+		}
+		{
+			FormData textContactsXmlPathLData = new FormData();
+			textContactsXmlPathLData.left =  new FormAttachment(0, 1000, 146);
+			textContactsXmlPathLData.top =  new FormAttachment(0, 1000, 17);
+			textContactsXmlPathLData.width = 304;
+			textContactsXmlPathLData.height = 13;
+			textContactsXmlPathLData.right =  new FormAttachment(1000, 1000, -71);
+			textContactsXmlPath = new Text(this, SWT.NONE);
+			textContactsXmlPath.setLayoutData(textContactsXmlPathLData);
+		}
+		{
+			labelContactsXmlPath = new Label(this, SWT.NONE);
+			FormData labelContactsXmlPathLData = new FormData();
+			labelContactsXmlPathLData.left =  new FormAttachment(0, 1000, 12);
+			labelContactsXmlPathLData.top =  new FormAttachment(0, 1000, 17);
+			labelContactsXmlPathLData.width = 122;
+			labelContactsXmlPathLData.height = 13;
+			labelContactsXmlPath.setLayoutData(labelContactsXmlPathLData);
+			labelContactsXmlPath.setText("Path of the contacts.xml:");
+		}
+		pack();
+		this.setSize(550, 200);
 	}
 	
-	public void Open()
-	{
-		createSShell();
-		Display d = sShell.getDisplay();
-		sShell.open();
-		while (!sShell.isDisposed()) 
-		{
-			if (!d.readAndDispatch ())
-			{
-				d.sleep (); 
-			}
-		} 
-		d.dispose ();
+	private void buttonRunWidgetSelected(SelectionEvent evt) {
+		Application app = new Application();
+		app.setContactsXmlPath(textContactsXmlPath.getText());
+		app.Run();
 	}
 	
-	class BrowseButtonListener implements SelectionListener
-	{
-		public void widgetSelected(SelectionEvent e)
-		{
-			System.out.println("widgetSelected()");
-		}
-		public void widgetDefaultSelected(SelectionEvent e)
-		{
-		}
-	}
-	
-	class RunButtonListener implements SelectionListener
-	{
-		public void widgetSelected(SelectionEvent e)
-		{
-			Application app = new Application();
-			app.setContactsXmlPath(ContactsTextBox.getText());
-			app.Run();
-		}
-		public void widgetDefaultSelected(SelectionEvent e)
-		{
-		}
+	private void buttonBrowseWidgetSelected(SelectionEvent evt) {
+		FileDialog fd = new FileDialog(this.getShell(), SWT.OPEN);
+        fd.setText("Open");
+        //...Local Settings\Application Data\Google\Picasa2\contacts\contacts.xml
+        fd.setFilterPath(System.getProperty("user.home"));
+        String[] filterExt = { "*.xml" };
+        fd.setFilterExtensions(filterExt);
+        String selected = fd.open();
+        if (selected != null) 
+        {
+        	textContactsXmlPath.setText(selected);	
+        }
 	}
 
 }
